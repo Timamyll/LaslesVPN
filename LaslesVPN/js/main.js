5b130222f1c89c133c8bb2f32013e54fd6916413
Elijah <62115378+elijahmylcev@users.noreply.github.com>
2022-06-14 23:16:07 +0300

    Hello
    Hello

---
+++ LaslesVPN/js/main.js
@@ -0,0 +1,8 @@
+$('.slider__winner').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+});
