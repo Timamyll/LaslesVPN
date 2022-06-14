d6267511be30387038dd98bf81780912af8e2e8f
Timamyll <104847527+Timamyll@users.noreply.github.com>
2022-06-14 04:52:34 +0300

    final no adaptiv

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.slider__winner').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/slider/03.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slider/04.svg" alt=""></button>',
+});
