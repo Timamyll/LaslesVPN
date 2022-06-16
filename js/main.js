bd567a5c3bc1bd0ee1bcadc6559dac7c9b74c870
Timamyll <104847527+Timamyll@users.noreply.github.com>
2022-06-16 20:18:21 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $('.slider__winner').slick({
-    dots: false,
+    dots: true,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/slider/03.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slider/04.svg" alt=""></button>',
+    slidesToScroll: 1,
+    // fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
 });
