5fac4214c1175ecb60103c55bc1c59db10f18b6b
Elijah Mylcev <elijahmylcev@gmail.com>
2022-06-16 23:02:36 +0300

    Refact

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $('.slider__winner').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     // fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
 });
