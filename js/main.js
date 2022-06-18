b038f86b35c60c1c42dcd43f26a1319d7e6aa331
Timamyll <104847527+Timamyll@users.noreply.github.com>
2022-06-18 20:47:03 +0300

    new commit

--- js/main.js
+++ js/main.js
@@ -4,8 +4,19 @@ $('.slider__winner').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     // fade: true,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        }
+    }]
 });
